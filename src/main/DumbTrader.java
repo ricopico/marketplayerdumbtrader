48590552a380fac16d74be8cd3a983764b92401d
ricopico <rich.caneba@gmail.com>
2016-02-17 15:50:06 -0800

    first commit

---
+++ src/main/DumbTrader.java
@@ -0,0 +1,12 @@
+package main;
+
+/**
+ * Created by rich on 2/17/16.
+ */
+public class DumbTrader {
+
+    public static void main(char[] args) {
+
+    }
+
+}
---
+++ src/test/DumbTraderTest.java
@@ -0,0 +1,7 @@
+package test;
+
+/**
+ * Created by rich on 2/17/16.
+ */
+public class DumbTraderTest {
+}
